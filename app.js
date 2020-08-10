f99f72bc886b1471d28faa1dc7e1dc3f86c9ba74
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2020-08-10 12:25:15 +1000

    Basic animations enabled

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const navbarLinks = document.getElementsByClassName("nav-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+// Scroll reveal animations
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "10rem",
+  delay: 300,
+  mobile: false,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+  mobile: false,
+});
